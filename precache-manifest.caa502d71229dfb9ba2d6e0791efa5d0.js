8f5493538e853e7119696c9d010c7e217b2ca6ed
Vladimir <bobuzh@gmail.com>
2020-03-17 14:17:54 +0200

    Updates

---
+++ precache-manifest.caa502d71229dfb9ba2d6e0791efa5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6902648e5fcf19d1de283caa3d589d19",
+    "url": "/react-todo-Udemy/index.html"
+  },
+  {
+    "revision": "f8a0e51f758baa407acf",
+    "url": "/react-todo-Udemy/static/css/main.36818fc4.chunk.css"
+  },
+  {
+    "revision": "3bcf6f28f3c0aceb453a",
+    "url": "/react-todo-Udemy/static/js/2.f482bfcb.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/react-todo-Udemy/static/js/2.f482bfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a0e51f758baa407acf",
+    "url": "/react-todo-Udemy/static/js/main.a5523351.chunk.js"
+  },
+  {
+    "revision": "efa625c0f194842f707a",
+    "url": "/react-todo-Udemy/static/js/runtime-main.c1034d40.js"
+  }
+]);
